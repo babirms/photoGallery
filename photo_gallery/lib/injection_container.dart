e705041acc4b7012230d1173fdd2d9687b65849a
babirms <bramos@id.uff.br>
2020-07-08 13:03:24 -0300

    realocate files

--- photo_gallery/lib/injection_container.dart
+++ photo_gallery/lib/injection_container.dart
@@ -1,4 +1,5 @@
 import 'package:get_it/get_it.dart';
+import 'package:photo_gallery/core/network/network_info.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 final sl = GetIt.instance;
@@ -7,6 +8,7 @@ void init() async {
   //! Features - Number Trivia
 
   //! Core
+  sl.registerLazySingleton<NetworkInfo>(() => NetworkInfoImpl(sl()));
 
   //! External
   final sharedPreferences = await SharedPreferences.getInstance();
